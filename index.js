a4f9578155e74333b989436cb04e3ea23ab5a983
bhavnesh1811 <erbhavnesharora@gmail.com>
2023-04-11 13:45:38 +0530

    cors added

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const { connection } = require("./Configs/db");
 const { PostRouter } = require("./Routes/Post.Route");
+const cors=require("cors");
 
 const server = express();
 server.use(express.json());
+
+server.use(cors({
+  origin:"*"
+}))
 
 server.get("/", (req, res) => {
   res.status(200).send("Api is working Fine");
